9ec49a2fd909b45d0810fe3fc70c945453aeafde
jehna <jesse.luoto@gmail.com>
2016-03-08 23:09:12 +0200

    Added: Selecting which album and files to download

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,27 +4,103 @@ var images;
 var percentLoadedRemaining;
 var zip;
 
+var selectedImages = [];
+
+var TARGET_IMAGE_WIDTH = 400;
+
 function loadedAllImages() {
-    //var $container = $('#select-images');
-    //var i = 0;
-    //var $row;
+    $('.progress').hide();
+    $('#select-images-to-download').show();
+    selectedImages = [];
+    $('#download-btn').hide();
+    
+    var $container = $('#select-images').show().html('');
+    var imgCol = 0;
+    var $row;
     
     images.forEach(function(image) {
-        /*if (i % 3 === 0) {
+        if (imgCol % 3 === 0) {
             $row = $('<div class="row" />').appendTo($container);
         }
         
+        var displayImage = image.images[0];
+        for (var i = 1; i < image.images.length; i++) {
+            if (Math.abs(displayImage.width - TARGET_IMAGE_WIDTH) < Math.abs(image.images[i] - TARGET_IMAGE_WIDTH)) {
+                displayImage = image.images[i];
+            }
+        }
+        
         var $col = $('<div class="col-md-4" />').appendTo($row);
-        $('<img />').attr('src', image.picture).appendTo($col);
+        $('<img class="img-rounded thumb" />')
+        .css('background-image', 'url('+displayImage.source+')')
+        .appendTo($col)
+        .click(function() {
+            if (selectedImages.indexOf(image) === -1) {
+                $col.addClass('selected');
+                selectedImages.push(image);
+            } else {
+                $col.removeClass('selected');
+                selectedImages.splice(selectedImages.indexOf(image), 1);
+            }
+            if (selectedImages.length > 0) {
+                $('#download-btn').show()
+                .text('Download ' + selectedImages.length + ' images');
+            } else {
+                $('#download-btn').hide();
+            }
+        });
         
-        i++;*/
+        imgCol++;
         
+        return false;
+    });
+    $row.appendTo($container);
+}
+
+function loadNextImages(url, paging) {
+    var params = { limit: 5 };
+    if (paging) {
+        params.after = paging.cursors.after;
+    } else {
+        images = [];
+        percentLoadedRemaining = 1;
+        $('.progress').show();
+        $('#progressbar').css({ width: '0%' }).show();
+    }
+    FB.api('/'+url+'/photos?fields=created_time,source,place{location{latitude,longitude}},name,id,images', 'GET', params, function(response) {
+        console.log(response);
+        images = images.concat(response.data);
+        
+        if (response.paging) {
+            setTimeout(function() {
+                loadNextImages(url, response.paging);
+            },1);
+            
+            percentLoadedRemaining *= 0.8; 
+        } else {
+            percentLoadedRemaining = 0;
+            loadedAllImages();
+        }
+        
+        $('#progressbar').css({ width: (1-percentLoadedRemaining)*100 + '%' });
+        
+    });
+}
+
+function downloadImages() {
+    selectedImages.forEach(function(image) {
         
         var exifData = {};
         if(image.created_time) {
             exifData.DateTime = exifData.DateTimeOriginal = image.created_time;
         }
         
+        var largestImage = image.images[0];
+        for (var i = 1; i < image.images.length; i++) {
+            if (image.images[i].width > largestImage.width) {
+                largestImage = image.images[i];
+            }
+        }
         
         zip = new JSZip();
         
@@ -61,71 +137,63 @@ function loadedAllImages() {
                 
                 var exifModified = piexif.insert(exifStr, 'data:image/jpeg;base64,' + base64);
                 
+                var fileName = (image.name ? image.name.substr(0,100).replace(/[\/\\]/g, '') : image.id);
+                var suffix = '';
+                var suffixNum = 0;
+                var ext = '.jpg';
+                
+                while (zip.files[fileName + suffix + ext]) {
+                    suffix = '_' + (++suffixNum);
+                }
+                
                 zip.file(
-                    (image.name ? image.name.substr(0,100).replace(/[\/\\]/g, '') : image.id) + '.jpg',
+                    fileName + suffix + ext,
                     exifModified.slice(23),
                     {
                         base64: true,
                         date: image.created_time ? new Date(image.created_time) : new Date()
                     }
                 );
+                
+                if (Object.keys(zip.files).length === selectedImages.length) {
+                    // All loaded, let's fire download
+                    location.href = 'data:application/zip;base64,' + zip.generate({ type: 'base64' });
+                }
             }
         };
         request.send(null);
-        
-        return false;
-    });
-    //$row.appendTo($container);
-}
-
-function loadNextImages(url, paging) {
-    var params = { limit: 5 };
-    if (paging) {
-        params.after = paging.cursors.after;
-    } else {
-        images = [];
-        percentLoadedRemaining = 1;
-        $('#progressbar').css({ width: '0%' }).show();
-    }
-    FB.api('/'+url+'/', 'GET', params, function(response) {
-        console.log(response);
-        images = images.concat(response.data);
-        
-        if (response.paging) {
-            setTimeout(function() {
-                loadNextImages(url, response.paging);
-            },1);
-            
-            percentLoadedRemaining *= 0.8; 
-        } else {
-            percentLoadedRemaining = 0;
-            loadedAllImages();
-        }
-        
-        $('#progressbar').css({ width: (1-percentLoadedRemaining)*100 + '%' });
-        
     });
 }
-
 
 $(function() {
-    
-    $('#load-images').click(function() {
-        var url = $('#input-url').val();
-        if(!/^(https?:\/\/)?(www.)?facebook.com\//.test(url)) {
-            console.log('Not valid URL');
-            return;
-        }
-        var id = /set=\w+\.(\d+)/.exec(url)[1];
-        loadNextImages(id);
+    $('#download-btn').click(function() {
+        downloadImages();
     });
 });
+
+function loadAlbums() {
+    FB.api('/me/albums', 'GET', null, function(response) {
+        console.log(response);
+        var $albums = $('#albums');
+        response.data.forEach(function(album) {
+            $('<option />')
+            .text(album.name)
+            .attr('value', album.id)
+            .appendTo($albums);
+        });
+        $albums.change(function() {
+            var albumID = $(this).val();
+            loadNextImages(albumID);
+        });
+    });
+}
 
 function checkLoginState() {
     FB.getLoginStatus(function(response) {
         if (response.status === 'connected') {
             $('.login').hide();
             $('#app').show();
+            loadAlbums();
         }
     });
 }
@@ -134,7 +202,7 @@ window.fbAsyncInit = function() {
     FB.init({
         appId      : '483759105134186',
         xfbml      : true,
-        version    : 'v2.3'
+        version    : 'v2.5'
     });
     
     checkLoginState();
